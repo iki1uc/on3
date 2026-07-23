255fd9f4b61524a252118c99873b9c69186c859f
iki1uc <entertrainer@web.de>
2026-07-23 11:59:05 +0200

    Create on3Connect.js

---
+++ on3Connect.js
@@ -0,0 +1,8 @@
+export function connectON3(){
+    return {
+        "ON3 → 9lu": "FRAME → PUBLIC",
+        "ON3 → 3te": "FRAME → OPERATOR",
+        "ON3 → META7": "FRAME → META",
+        "ON3 → TP9": "FRAME → NEXT"
+    };
+}
